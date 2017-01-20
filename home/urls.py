30872198a3dfc9c2b7b33b6718d17244f2e5bc9f
marcelodeolive1ra <marcelodeoliveira@outlook.com>
2017-01-20 20:31:40 -0200

    Implementa login e logout + mudanças de layout na tela de login

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,9 @@ from home import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^home/$', views.index, name='home'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
     url(r'^base_configuracoes/$', views.base_configuracoes, name='base_configuracoes'),
     url(r'^base_aplicar_dinamica/$', views.base_aplicar_dinamica, name='base_aplicar_dinamica'),
 ]
